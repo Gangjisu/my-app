ba4ac383cf568524ce4de7c5610f17c111fe988e
Gangjisu <51405284+Gangjisu@users.noreply.github.com>
2025-06-06 11:11:45 +0900

    Add match search UI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/이터널 리턴 전적 검색/i);
+  expect(headerElement).toBeInTheDocument();
 });
